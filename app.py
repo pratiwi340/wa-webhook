0d725e435ed187151836622fecdbb5cfbf118254
pratiwi340 <bpratiwi340@gmail.com>
2025-07-09 20:17:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-services:
-  - type: web
-    name: wa-webhook
-    env: python
-    plan: free
-    buildCommand: ""
-    startCommand: "python app.py"
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Webhook WA aktif!'
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.json
+    print("Pesan diterima:", data)
+    return {'status': 'OK'}, 200
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
